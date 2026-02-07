e048ef66b4a35af898b8f060321b6b80c445cb2c
Jeff Xie <jeffxie4@gmail.com>
2026-02-07 17:14:49 -0500

    Add a test-service to Docker Compose

--- web/test/e2e/test_app_redis_http.py
+++ web/test/e2e/test_app_redis_http.py
@@ -13,5 +13,5 @@ def test_should_update_redis(redis_client, flask_url):
 
     # Then
     assert response.status_code == 200
-    assert response.text == "This page has been seen 5 times."
+    assert response.text == "This page has been seen 1 times."
     assert redis_client.get("page_views") == b"5"
